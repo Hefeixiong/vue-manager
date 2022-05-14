85742eb54b1b39529f1887387276ff0e6cf5aba7
hefeixiong <1074535830@qq.com>
2022-05-14 17:28:37 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import {resolve} from 'path'
+import * as path from 'path';
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -19,7 +20,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
