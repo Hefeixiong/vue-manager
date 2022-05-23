7940bdf35b6d21c9b45eb7088ea51230b3cbbe65
hefeixiong <1074535830@qq.com>
2022-05-23 11:22:41 +0800

    保存代码

--- src/view/UserManage.vue
+++ src/view/UserManage.vue
@@ -22,24 +22,22 @@
   </el-table></template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
+
+import { computed, onMounted, ref } from 'vue'
 import {mapActions, useStore} from 'vuex';
 import {UserState} from '../store/modules/user';
 
 
 const store = useStore()
 
+store.dispatch('getInfo')
+console.log(store.state.user.name)
 //tableDate是Array
 const tableData = [{
   name: store.state.user.name,
   date: store.state.user.date,
   address: store.state.user.address
 }]
-
-
-  store.dispatch('getInfo')
-
-
 const search = ref('')
 
 const filterTableData = computed(() =>
