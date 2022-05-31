4bdd315591a0606d884a8aee2659f25826211e58
hefeixiong <1074535830@qq.com>
2022-05-31 23:21:08 +0800

    新增todo.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import {createStore, createLogger} from 'vuex';
 
 import user from './modules/user'
+import todo from './modules/todo';
 
 
 export const store = createStore({
   modules: {
-    user
+    user,
+    todo
   }
 })
---
+++ src/store/modules/todo.ts
@@ -0,0 +1,29 @@
+import User from "../../api/user";
+import {add, get} from "../../utils/localStorage"
+
+
+
+const state = {
+
+};
+
+const getters = {
+
+};
+
+const mutations = {
+ 
+};
+
+const actions = {
+
+}
+
+
+export default {
+  namespace: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
